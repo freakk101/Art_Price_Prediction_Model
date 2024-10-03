6833ba3301173ce7e300db64f21cb5a71befe070
Abdul Mateen shaikh <matin1106@gmail.com>
2024-10-03 19:26:26 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,12 +4,12 @@
 sudo apt-get update
 sudo apt-get install -y wget unzip xvfb libxi6 libgconf-2-4
 
-# Download and install Google Chrome
+# Install Google Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt-get install -y ./google-chrome-stable_current_amd64.deb
 rm google-chrome-stable_current_amd64.deb
 
-# Download and install ChromeDriver
+# Install ChromeDriver
 CHROME_VERSION=$(google-chrome --version | grep -oP '\d+\.\d+\.\d+')
 wget -N https://chromedriver.storage.googleapis.com/$CHROME_VERSION/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip -d /usr/local/bin/
